fc4bafab48ef042fb3db4244cad44f6fb00a28bd
Mikkel Breum <mail@mikkelbreum.com>
2014-01-08 12:53:20 +0100

    Update vvv-init.sh

--- vvv-init.sh
+++ vvv-init.sh
@@ -1,30 +1,27 @@
 # Init script for site provisioner
 
 # import the config to keep it DRY
-. config.sh;
+# . config.sh;
 
-echo "Commencing setup of $NEWSITENAME"
-
-touch "vvv-hosts"
-echo "${NEWSITENAME}.de >" | vvv-hosts
+echo "Commencing setup of sandbox"
 
 # Make a database, if we don't already have one
 echo "Creating database (if it's not already there)"
-mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS $NEWDBNAME"
-mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON $NEWDBNAME.* TO wp@localhost IDENTIFIED BY 'wp';"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS sandbox"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON sandbox.* TO wp@localhost IDENTIFIED BY 'wp';"
 
 # Download WordPress
 if [ ! -d htdocs ]
 then
-	echo "Installing WordPress for $NEWSITENAME using WP CLI"
+	echo "Installing WordPress for sandbox using WP CLI"
 	mkdir htdocs
 	cd htdocs
 	wp core download 
-	wp core config --dbname="$NEWDBNAME" --dbuser=wp --dbpass=wp --dbhost="localhost"
-	wp core install --url="$NEWSITENAME.dev" --title="$NEWSITENAME" --admin_user=admin --admin_password=password --admin_email=demo@example.com
+	wp core config --dbname="sandbox" --dbuser=wp --dbpass=wp --dbhost="localhost"
+	wp core install --url="sandbox.dev" --title="sandbox" --admin_user=admin --admin_password=password --admin_email=demo@example.com
 	cd ..
 fi
 
 # The Vagrant site setup script will restart Nginx for us
 
-echo "$NEWSITENAME now installed";
+echo "sandbox now installed";
